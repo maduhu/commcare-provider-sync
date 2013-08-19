4c247302bae80664558a9a75caf3a64322c4ea62
Ashish Mishra <ashishkm@thoughtworks.com>
2013-08-19 15:16:51 +0530

    Ashish|#00|Adding bundle IT and fixed log4j.xml to provide log.dir

--- src/test/java/org/motechproject/commcare/provider/sync/CommCareProviderSyncBundleIT.java
+++ src/test/java/org/motechproject/commcare/provider/sync/CommCareProviderSyncBundleIT.java
@@ -2,7 +2,7 @@ package org.motechproject.commcare.provider.sync;
 
 import org.motechproject.testing.osgi.BaseOsgiIT;
 
-public class CommCareSyncBundleIT extends BaseOsgiIT {
+public class CommCareProviderSyncBundleIT extends BaseOsgiIT {
     public void testBundle() throws Exception {
         testOsgiPlatformStarts();
     }
